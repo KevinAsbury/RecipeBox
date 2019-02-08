64dc962a2906466ea9f0aa64e71cbbb9a9586ef4
Kevin Asbury <kasbury@pursuitky.com>
2019-02-08 18:43:00 -0500

    Added recipe book component

---
+++ src/app/recipe-book/recipe-book.component.ts
@@ -0,0 +1,13 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-recipe-book',
+    templateUrl: './recipe-book.component.html'
+})
+export class RecipeBookComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
