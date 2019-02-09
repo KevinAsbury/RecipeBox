12fac49e7ad8e5f3e51db3fe845d96173ae16612
Kevin Asbury <kasbury@pursuitky.com>
2019-02-08 19:35:15 -0500

    Added recipe list subcomponent to recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
+    RecipesComponent,
     RecipeDetailComponent,
-    RecipesComponent
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
