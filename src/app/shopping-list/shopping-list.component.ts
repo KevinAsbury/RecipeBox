2a406dd11b4970b51d7f6f74366a19afb02565b4
Kevin Asbury <kasbury@pursuitky.com>
2019-02-18 16:22:58 -0500

    Added ingredients to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Bun', 1),
+    new Ingredient('Hot Dog', 1),
+    new Ingredient('Tomato', 5),
+    new Ingredient('Onion', 1),
+    new Ingredient('Ground Beef', 2)
+  ];
+
   constructor() { }
 
   ngOnInit() { }
