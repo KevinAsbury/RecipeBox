aa516fd9f675c02589f8da540800ab38db7473a8
Kevin Asbury <kasbury@pursuitky.com>
2019-02-18 16:01:48 -0500

    Built a basic recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
-    selector: 'app-recipe-list',
-    templateUrl: './recipe-list.component.html'
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html'
 })
-export class RecipeListComponent implements OnInit{
-    constructor() {}
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Bro Dogs', 'Dogs bruh', 'https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg'),
+    new Recipe('Bro Burgs', 'Burgs bruh', 'https://media-cdn.tripadvisor.com/media/photo-s/11/66/3d/41/img-20171117-wa0002-largejpg.jpg')
+  ];
 
-    ngOnInit() {}
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 }
